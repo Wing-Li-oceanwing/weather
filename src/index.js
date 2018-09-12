b89385987577b0136451b44b9e5c3c47122d8445
wing <wing.li@oceanwing.com>
2018-09-12 20:38:14 +0800

    first commit1

--- src/index.js
+++ src/index.js
@@ -2,17 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
-import {createStore, applyMiddleware } from 'redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
+import {applyMiddleware, createStore} from 'redux';
+import {composeWithDevTools} from 'redux-devtools-extension';
 import {logger} from 'redux-logger';
-import rootReducer from './reducers'
+import rootReducer from './reducers';
+import {Provider} from 'react-redux';
 
 
 const store = createStore(
     rootReducer,
     composeWithDevTools(
         applyMiddleware(logger)
-)
+    )
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <Provider store={store}>git
+        <App />
+    </Provider>, document.getElementById('root'));
 registerServiceWorker();
